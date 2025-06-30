859bfa76628970af6a58019828e2c8ef00726e9c
ZEALOT <phinehasdscience@gmail.com>
2025-06-30 14:20:08 +0000

    feat: add task deletioon with configuration and backend cleanup

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -12,6 +12,17 @@ interface TaskCardProps {
 const TaskCard =({task}: TaskCardProps) => {
     const [isEditing, setIsEditing] = useState(false);
 
+    const handleDelete  = async() =>{
+        const confirmed = confirm("Are you sure you want to delete this task?");
+        if(!confirmed) return;
+        try {
+            await fetch(`api/tasks/${task._id}`,{method: "DELETE",});
+            window.location.reload();
+
+        } catch (error) {
+            console.error("Error deleting task", error);
+        }
+    }
 
     return (
         <>
@@ -22,16 +33,29 @@ const TaskCard =({task}: TaskCardProps) => {
                         <p className="text-gray-600">{task.description}</p>
                         <p className="text-sm text-gray-400 mt-1">Status: {task.status}</p>
                     </div>
+
+                    <div className="flex gap-x-2">
                     <button     
                         onClick={() => setIsEditing(true)}
                         className="bg-blue-100 hover:bg-blue-200 px-3 py-1 rounded text-sm"
                     >
                         Edit
                     </button>
+
+                    <button 
+                        onClick={handleDelete}
+                        className="bg-red-200 hover:bg-red-400 px-3 py-1 rounded text-sm"
+                    >
+                        Delete
+                    </button>
+                    </div>
+
+
                 </div>
             </div>   
             <Modal isOpen = {isEditing} onClose={() => setIsEditing(false)}>
                 <EditTaskForm task={task} onClose={() =>setIsEditing(false)} />
+                
             </Modal>
         </>
     )
